6cffd35125a717788e692c9a2d7fac5fa8479eff
Agustin Cabero <agustincabero@gmail.com>
2023-02-23 23:41:29 -0300

    Morgan added to app

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) Middlewares
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,6 +22,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) Route handlers
 
 const getAllTours = (req, res) => {
   res.status(200).json({
@@ -113,6 +120,8 @@ const deleteTour = (req, res) => {
 //app.patch('/api/v1/tours/:id', updateTour);
 //app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) Routes
+
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -120,6 +129,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) Start server
 
 const port = 8000;
 app.listen(port, () => {
